9704fac0dc5e6c16472dae806094873f08c68d00
Miguel <miguevalencia29@hotmail.com>
2020-08-24 19:05:31 -0500

    Add Implemetation of RandomGnerator but using the class and not the interfaces to use dependendy injection

--- social-multiplication/src/main/java/microservices/book/multiplication/service/RandomGeneratorServiceImpl.java
+++ social-multiplication/src/main/java/microservices/book/multiplication/service/RandomGeneratorServiceImpl.java
@@ -1,5 +1,20 @@
 package microservices.book.multiplication.service;
 
-public class RandomGeneratorServiceImpl {
+import java.util.Random;
 
+import org.springframework.stereotype.Service;
+
+@Service
+public class RandomGeneratorServiceImpl implements RandomGeneratorService {
+	
+	final static int MINIMUM_FACTOR = 11;
+	final static int MAXIMUM_FACTOR = 99;
+
+	@Override
+	public int generateRandomFactor() {
+		return new Random().nextInt((MAXIMUM_FACTOR - MINIMUM_FACTOR) + 1) + MINIMUM_FACTOR;
+	}
+
+	
+	
 }
