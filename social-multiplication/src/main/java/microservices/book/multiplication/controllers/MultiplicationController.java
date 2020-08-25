7a42130a0d99b34be8869198fe65bc2e63355597
Miguel <miguevalencia29@hotmail.com>
2020-08-25 15:52:33 -0500

    Complementing the Multiplication Controller and test it

--- social-multiplication/src/main/java/microservices/book/multiplication/controllers/MultiplicationController.java
+++ social-multiplication/src/main/java/microservices/book/multiplication/controllers/MultiplicationController.java
@@ -1,11 +1,15 @@
 package microservices.book.multiplication.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import microservices.book.multiplication.domain.Multiplication;
 import microservices.book.multiplication.service.MultiplicationService;
 
 @RestController
+@RequestMapping("/multiplications")
 public class MultiplicationController {
 	
 	private final MultiplicationService multiplicationService;
@@ -15,5 +19,9 @@ public class MultiplicationController {
 		this.multiplicationService = multiplicationService;
 	}
 	
+	@GetMapping("/random")
+	Multiplication getRandomMultiplication() {
+		return multiplicationService.createRandomMultiplication();
+	}
 	
 }
