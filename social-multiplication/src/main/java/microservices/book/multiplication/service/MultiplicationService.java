3862e49c731fefa0d75625e44705572b699fff14
Miguel <miguevalencia29@hotmail.com>
2020-08-25 10:09:11 -0500

    add one method to the MultiplicationService interface

--- social-multiplication/src/main/java/microservices/book/multiplication/service/MultiplicationService.java
+++ social-multiplication/src/main/java/microservices/book/multiplication/service/MultiplicationService.java
@@ -1,9 +1,12 @@
 package microservices.book.multiplication.service;
 
 import microservices.book.multiplication.domain.Multiplication;
+import microservices.book.multiplication.domain.MultiplicationResultAttempt;
 
 public interface MultiplicationService {
 	
 	Multiplication createRandomMultiplication();
+	
+	boolean checkAttempt(final MultiplicationResultAttempt resultAttempt);
 	
 }
